845f26d58829831aca41985f755554cbf2d06a25
Tom Souillard <tom.souillard@gmail.com>
2023-09-30 21:07:50 +0200

    [feat] Add method for descriptive analysis and correlation matrix
    - Implemented a static method  to perform descriptive analysis.
    - The method generates and returns descriptive statistics using  and a correlation matrix using .
    - Updated the example usage to include data analysis and print the results.

--- data_explorer.py
+++ data_explorer.py
@@ -13,9 +13,18 @@ class DataExplorer:
         data = data.fillna(data.mean())
         return data
 
+    @staticmethod
+    def analyze_data(data):
+        description = data.describe()
+        correlation = data.corr()
+        return description, correlation
+
 if __name__ == "__main__":
     file_path = 'examples/example_data.csv'
     explorer = DataExplorer()
 
     data = explorer.load_data(file_path)
     clean_data = explorer.clean_data(data)
+    description, correlation = explorer.analyze_data(clean_data)
+    print("Descriptive Statistics:\n", description)
+    print("Correlation Matrix:\n", correlation)
